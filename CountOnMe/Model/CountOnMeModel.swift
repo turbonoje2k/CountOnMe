16b5462bacc8ca489fab2455ff9f5a9acff03c3a
Noje <9hnoje@gmail.com>
2021-07-16 21:05:16 +0200

    fix MVC works

--- CountOnMe/Model/CountOnMeModel.swift
+++ CountOnMe/Model/CountOnMeModel.swift
@@ -7,3 +7,141 @@
 //
 
 import Foundation
+
+protocol CalculatorDelegate {
+    func displayAlert(message: String)
+    func didReceiveData(data: String)
+    
+}
+
+class CalculatorModel {
+    
+    var delegate: CalculatorDelegate?
+    func sendToControler(data: String) {
+        delegate?.didReceiveData(data: data)
+    }
+    var textView = ""
+    var elements: [String] {
+        return textView.split(separator: " ").map { "\($0)" }
+    }
+    
+    func expressionIsCorrect(elements: [String]) ->  Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    func expressionHaveEnoughElement(elements: [String])-> Bool {
+        return elements.count >= 3
+    }
+    
+    func canAddOperator(elements: [String]) -> Bool {
+        return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "/"
+    }
+    
+    func expressionHaveResult(elements: String) ->  Bool {
+        return textView.firstIndex(of: "=") != nil
+    }
+    
+    func tappedAddition() {
+        if canAddOperator(elements: elements) {
+            textView += " + "
+        } else {
+            print("error: operand already exist")
+        }
+        return sendToControler(data: "+")
+    }
+    
+    func tappedSubstraction() {
+        if canAddOperator(elements: elements) {
+            textView += " - "
+        } else {
+            print("error: operand already exist")
+        }
+        return sendToControler(data: "-")
+    }
+    
+    func tappedMultiplication() {
+        if canAddOperator(elements: elements) {
+            textView += " * "
+        } else {
+            print("error: operand already exist")
+        }
+        return sendToControler(data: "*")
+    }
+    
+    func tappedDivision() {
+        if canAddOperator(elements: elements) {
+            textView += " / "
+        } else {
+            print("error: operand already exist")
+        }
+        return sendToControler(data: "/")
+    }
+    
+    func addStringNumber(number: String) {
+        if expressionHaveResult(elements: textView) {
+            textView = ""
+        }
+        textView += number
+        sendToControler(data: number)
+    }
+    
+    func tappedEqual()  {
+        guard expressionIsCorrect(elements: elements) else {
+            delegate?.displayAlert(message: "Expression not correct")
+            return
+        }
+        guard expressionHaveEnoughElement(elements: elements) else {
+            delegate?.displayAlert(message: "not Enought elements")
+            return
+        }
+        calculate()
+    }
+    
+    func calculate() {
+        var operationsToReduce = elements
+        
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            var place = 0
+            // priority for * or / operator
+            if let index = operationsToReduce.firstIndex(where: { $0 == "*" || $0 == "/" }) {
+                // to force priority to left operator, index -1
+                place = index - 1
+            }
+            guard let left = Double(operationsToReduce[place]) else { return }
+            let operand = operationsToReduce[place + 1]
+            guard let right = Double(operationsToReduce[place + 2]) else { return }
+            
+            let result: Double
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            case "*": result = left * right
+            case "/": result = division(left: left, right: right)
+            default: fatalError("Unknown operator !")
+            }
+            
+            //            operationsToReduce = Array(operationsToReduce(3))
+            for _ in 1...3 {
+                operationsToReduce.remove(at: place)
+            }
+            operationsToReduce.insert("\(result)", at: place)
+        }
+        
+//        textView.append(" = \(operationsToReduce.first!)")
+        textView += " = \(operationsToReduce.first ?? "Error")"
+        sendToControler(data: textView)
+    }
+    
+    func division(left: Double, right: Double) -> Double {
+        //        guard right == 0 else { return -1 }
+            if right == 0 {
+                textView.append("not void ")
+            }
+            let result = left / right
+            return result
+        }
+}
+
+
+
--- CountOnMe/ViewController.swift
+++ CountOnMe/ViewController.swift
@@ -12,32 +12,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
     
-    var elements: [String] {
-        return textView.text.split(separator: " ").map { "\($0)" }
-    }
+    var calculator = CalculatorModel()
     
-    // Error check computed variables
-    var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-"
-    }
-    
-    var expressionHaveEnoughElement: Bool {
-        return elements.count >= 3
-    }
-    
-    var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "/"
-    }
-    
-    
-    var expressionHaveResult: Bool {
-        return textView.text.firstIndex(of: "=") != nil
-    }
-    
-    // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calculator.delegate = self
     }
     
     func warnigRepeatOperator() {
@@ -46,102 +26,43 @@ class ViewController: UIViewController {
         self.present(alertVC, animated: true, completion: nil)
     }
     
-    
     // View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
-        guard let numberText = sender.title(for: .normal) else {
-            return
-        }
-        
-        if expressionHaveResult {
-            textView.text = ""
-        }
-        
-        textView.text.append(numberText)
+
+        guard let titleButton = sender.title(for: .normal) else { return }
+        calculator.addStringNumber(number: titleButton)
     }
     
     @IBAction func tappedAdditionButton(_ sender: UIButton) {
-        if canAddOperator {
-            textView.text.append(" + ")
-        } else {
-            warnigRepeatOperator()
-        }
+        calculator.tappedAddition()
     }
     
     @IBAction func tappedSubstractionButton(_ sender: UIButton) {
-        if canAddOperator {
-            textView.text.append(" - ")
-        } else {
-            warnigRepeatOperator()
-        }
+
+        calculator.tappedSubstraction()
     }
     @IBAction func tappedMultiplicationButton(_ sender: UIButton) {
-        if canAddOperator {
-            textView.text.append(" * ")
-        } else {
-            warnigRepeatOperator()
-        }
+        calculator.tappedMultiplication()
     }
     @IBAction func tappedDivisionButton(_ sender: UIButton) {
-        if canAddOperator {
-            textView.text.append(" / ")
-        } else {
-            warnigRepeatOperator()
-        }
+
+        calculator.tappedDivision()
     }
     
     @IBAction func tappedEqualButton(_ sender: UIButton) {
-        guard expressionIsCorrect else {
-            return warnigRepeatOperator()
-        }
-        
-        guard expressionHaveEnoughElement else {
-            return warnigRepeatOperator()
-        }
-        
-        // Create local copy of operations
-        var operationsToReduce = elements
-        
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            var place = 0
-            // priority for * or / operator
-            if let index = operationsToReduce.firstIndex(where: { $0 == "*" || $0 == "/" }) {
-                // to force priority to left operator, index -1
-                place = index - 1
-            }
-            guard let left = Double(operationsToReduce[place]) else { return }
-            let operand = operationsToReduce[place + 1]
-            guard let right = Double(operationsToReduce[place + 2]) else { return }
-            
-            let result: Double
-            switch operand {
-            case "+": result = left + right
-            case "-": result = left - right
-            case "*": result = left * right
-            case "/": result = division(left: left, right: right)
-            default: fatalError("Unknown operator !")
-            }
-            
-            //            operationsToReduce = Array(operationsToReduce(3))
-            for _ in 1...3 {
-                operationsToReduce.remove(at: place)
-            }
-            operationsToReduce.insert("\(result)", at: place)
-        }
-        
-        textView.text.append(" = \(operationsToReduce.first!)")
+        calculator.tappedEqual()
+    }
+}
+
+//EXTENSION
+extension ViewController: CalculatorDelegate {
+    func displayAlert(message: String) {
+        print("alert")
+    }
+    
+    func didReceiveData(data: String) {
+        textView.text = calculator.textView
     }
     
     
-    func division(left: Double, right: Double) -> Double {
-        //        guard right == 0 else { return -1 }
-            if right == 0 {
-                textView.text.append("not void ")
-            }
-            let result = left / right
-            return result
-        }
-
 }
-
